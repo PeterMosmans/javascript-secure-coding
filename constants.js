d021a3156b2b045b8f3608490ed073478e17194e
Peter Mosmans <support@go-forward.net>
2025-01-28 16:16:14 +0100

    feat: move authorization of token to authorization page

--- constants.js
+++ constants.js
@@ -25,28 +25,28 @@ export const SELECT = `<button id="to-innerhtml">set input as innerHTML</button>
         <button id="to-sanitized">sanitize input</button>
         <button id="to-sanitized-text">sanitize as plaintext</button>
         <button id="validate-email">Validate as email address</button>
-        <a href='login'>Go to login page</a>
-        <a href='forms'>Go to forms page</a>`
+        <a href='authentication'>Go to authentication page</a>
+        <a href='authorization'>Go to authorization page</a>`
 
 export const INPUT = `<textarea id="input" class="multiline" wrap="soft"></textarea>`
 
 export const LOGIN = `<input type="text" id="username" placeholder="username" required>
-      <input type="password" id="password" placeholder="password" required>
-      <select id="permission">
-        <option value="create">create access</option>
-        <option value="read">read access</option>
-        <option value="delete">delete access</option>
-        <option value="superpower">superpower access</option>
-      </select>`
+      <input type="password" id="password" placeholder="password" required>`
 
 export const PERMISSIONS = `<button id="login">Login (reset session)</button>
-      <button id="authorize">Validate permissions using token</button>
      <a href="/">Go to input page</a>
-      <a href="forms">Go to forms page</a>`
+     <a href='authorization'>Go to authorization page</a>`
 
-export const FORM = `<form method="POST" action="action">
+export const FORM = `<form method="POST" action="authorization">
           <input type="text" name="action" value="do this">
           <button type="submit">Perform action</button>
         </form>
-        <a href='login'>Go to login page</a>
+        <select id="permission">
+          <option value="create">create access</option>
+          <option value="read">read access</option>
+          <option value="delete">delete access</option>
+          <option value="superpower">superpower access</option>
+        </select>
+        <button id="authorize">Validate access permissions</button>
+        <a href='authentication'>Go to authentication page</a>
         <a href='/'>Go to input page</a>`
